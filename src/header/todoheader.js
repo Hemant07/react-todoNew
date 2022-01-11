1c8e5255f17b883fdccb54d84b2710f9b8e8e978
Hemant07 <hemantkhinchi84@gmail.com>
2022-01-11 23:56:13 +0530

    1 file modified

--- src/header/todoheader.js
+++ src/header/todoheader.js
@@ -5,11 +5,19 @@ import './todoheader.css';
 // };
 
 const HeaderTodo = () => {
+  const titleChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
   return (
     // <form id="myDIV" className="header" onClick={newElementHandler}>
     <form id="myDIV" className="header">
       <h2>My To Do List</h2>
-      <input type="text" id="myInput" placeholder="Title..." />
+      <input
+        type="text"
+        id="myInput"
+        placeholder="Title..."
+        onChange={titleChangeHandler}
+      />
       <button type="submit" className="addBtn">
         Add
       </button>
