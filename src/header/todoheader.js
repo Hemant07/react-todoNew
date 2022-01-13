8ef65999e2bf91c1b00862cba92edb38b795b96d
Hemant07 <hemantkhinchi84@gmail.com>
2022-01-13 10:27:29 +0530

    1 file modified

--- src/header/todoheader.js
+++ src/header/todoheader.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './todoheader.css';
 // newElementHandler = (e) => {
 //   return '';
 // };
 
 const HeaderTodo = () => {
+  const [enteredTodo, setNewTodo] = useState('');
+
   const titleChangeHandler = (event) => {
-    console.log(event.target.value);
+    setNewTodo(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const todoData = {
+      newTitle: enteredTodo,
+    };
+    console.log(todoData);
+    setNewTodo('');
   };
   return (
     // <form id="myDIV" className="header" onClick={newElementHandler}>
-    <form id="myDIV" className="header">
+    <form id="myDIV" className="header" onSubmit={submitHandler}>
       <h2>My To Do List</h2>
       <input
         type="text"
         id="myInput"
+        value={enteredTodo}
         placeholder="Title..."
         onChange={titleChangeHandler}
       />
