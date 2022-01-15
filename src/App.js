2c86de58221215f2385d422360bba68fe8728023
Hemant07 <hemantkhinchi84@gmail.com>
2022-01-15 18:00:26 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './style.css';
 import HeaderTodo from './header/todoheader';
 import TodoListHolder from './todo_list/todoListHolder';
 export default function App() {
+  const todoItems = [
+    { name: 'Hit the gym' },
+    { name: 'Pay bills' },
+    { name: 'Meet George' },
+    { name: 'Buy eggs' },
+    { name: 'Read a book' },
+    { name: 'Organize office' },
+  ];
   const onSaveHandler = (enteredDataHandler) => {
     const todoListData = {
       ...enteredDataHandler,
@@ -12,7 +20,7 @@ export default function App() {
   return (
     <div>
       <HeaderTodo onSaveTodoData={onSaveHandler} />
-      <TodoListHolder />
+      <TodoListHolder todoTitle={todoItems.name} />
     </div>
   );
 }
--- src/todo_list/todoListHolder.js
+++ src/todo_list/todoListHolder.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import './todoListHolder.css';
 import Todolisttems from './todoitems';
-const TodoListHolder = () => {
-  const todoItems = [
-    { name: 'Hit the gym' },
-    { name: 'Pay bills' },
-    { name: 'Meet George' },
-    { name: 'Buy eggs' },
-    { name: 'Read a book' },
-    { name: 'Organize office' },
-  ];
-
+const TodoListHolder = (props) => {
   return (
     <ul id="myUL">
-      <Todolisttems title={todoItems[0].name} />
+      <Todolisttems {props.todoItems[0].name} />
       <Todolisttems title={todoItems[1].name} />
       <Todolisttems title={todoItems[2].name} />
       <Todolisttems title={todoItems[3].name} />
