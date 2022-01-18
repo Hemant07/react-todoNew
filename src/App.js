5be907103672362a963c7eb80b9c46c161d773f7
Hemant07 <hemantkhinchi84@gmail.com>
2022-01-18 13:58:52 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import './style.css';
 import HeaderTodo from './header/todoheader';
 import TodoListHolder from './todo_list/todoListHolder';
 export default function App() {
-  const [todoItems,setTodoItems] = useState([
-    { name: 'Hit the gym' },
-    { name: 'Pay bills' },
-    { name: 'Meet George' },
-    { name: 'Buy eggs' },
-    { name: 'Read a book' },
-    { name: 'Organize office' },
-
+  const [todoItems, setTodoItems] = useState([
+    { id: 1, name: 'Hit the gym' },
+    { id: 2, name: 'Pay bills' },
+    { id: 3, name: 'Meet George' },
+    { id: 4, name: 'Buy eggs' },
+    { id: 5, name: 'Read a book' },
+    { id: 6, name: 'Organize office' },
   ]);
   const [inputItem, setInputItem] = useState('Hello world ');
   const onSaveHandler = (name) => {
@@ -18,9 +17,9 @@ export default function App() {
       ...todoItems,
       {
         id: Math.random().toString(),
-        name
-      }
-    ])
+        name,
+      },
+    ]);
   };
   return (
     <div>
